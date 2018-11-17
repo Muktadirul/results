3bfee3f9207bb71285e31c25e6af6f38fd6d7672
Muktadirul <raiancse@gmail.com>
2018-11-17 14:00:57 +0600

    check results

---
+++ resultAstral/11-texon/estimated_15genes_strongILS/FNRATE_Astral/out_ast.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast8_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast17_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast13_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast14_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast5_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast1_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast12_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast20_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast11_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast15_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast3_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast16_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast2_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast10_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast7_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast6_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast18_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast9_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast19_stripd
+python tools/spruce-1.0/getFpFn.py -t dataset/11-texon/estimated_15genes_strongILS/model/model_tree_trimmed -e resultAstral/11-texon/estimated_15genes_strongILS/Estimated_QS_Score_SP_TREE/Stripped/SP_ast4_stripd
